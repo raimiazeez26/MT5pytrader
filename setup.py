5df6d1056596373b2d8b30f397beb96de391f897
Raimi Azeez Babatunde <61598894+raimiazeez26@users.noreply.github.com>
2022-08-09 16:04:56 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 
 setup(
     name='MT5pytrader',
-    version='0.24',
-    description = "MT5 Trader",
+    version='0.61',
+    description = "MT5pytrader",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     author="Raimi Azeez Babatunde",
     author_email='raimiazeez26@gmail.com',
     packages=['MT5pytrader'],
+    url='https://github.com/raimiazeez26/MT5pytrader',
     keywords=['MT5pytrader', 'python', 'Metatrader5', 'MT5', 'algotrading',
              'autroading'],
     install_requires=[
-          'MetaTrader5>=5.0.37',
+          'MetaTrader5',
       ],
     zip_safe = False
 
 )
-
